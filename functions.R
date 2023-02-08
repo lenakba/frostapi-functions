a1e2a64dd2365aba4c7f28e2b779c8e007469050
Lena Kristin Bache-Mathiesen <lenakb@nih.no>
2023-02-08 17:32:48 +0100

    Commiting initial script that is to include the functions. Read R packages.

---
+++ functions.R
@@ -0,0 +1,7 @@
+# Script to load functions get_all_vestland_sensors()
+# and get_coordnates_by_id()
+# needed for obtaining Vestland-sensors, and
+# finding the latitude, longitude coords of any sensor
+
+library(jsonlite) # reading JSON files
+library(tidyverse) # datawrangling
